478dd87a34699031beae93716c7c88c796ce0e6a
Eslam Mahmoud <espython85@gmail.com>
2024-10-07 12:00:26 +0300

    Time: 82 ms (58.42%), Space: 60.3 MB (34.53%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -3,9 +3,9 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
+var rotate = function (nums, k) {
     const len = nums.length
-  nums.push(...nums.splice(0, (-k % len + len) % len))
-  return nums
-    
+    nums.push(...nums.splice(0, (-k % len + len) % len))
+    return nums
+
 };
