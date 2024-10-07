f672ae25b094f393105d9f2ed392408db6343d6f
Eslam Mahmoud <espython85@gmail.com>
2024-10-07 14:02:18 +0300

    Time: 72 ms (67.77%), Space: 59.4 MB (28.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,19 +2,14 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {
- let curMin = prices[0]
- let curMax = 0 
- for(let cur of prices){
-    curMin = Math.min(cur,curMin)
-    curMax = Math.max(curMax,cur-curMin)
- }
+var maxProfit = function(prices) {
+    let currentMin = prices[0]
+    let currentMax = 0
+    for(let price of prices){
+        currentMin = Math.min(price,currentMin)
+        currentMax = Math.max(currentMax,price -currentMin)
+    }
 
-return curMax
-
-
-
-
-
-
+    return currentMax
+    
 };
