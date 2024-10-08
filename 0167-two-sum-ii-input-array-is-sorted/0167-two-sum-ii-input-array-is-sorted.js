196cff334329a8fa09ab27f5b686f65abb67c7a8
Eslam Mahmoud <espython85@gmail.com>
2024-10-08 11:43:48 +0300

    Time: 59 ms (41.24%), Space: 49.3 MB (97.88%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,17 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function (numbers, target) {
-    const map = new Map()
-    let i = 0
-    while (i < numbers.length) {
-        let num1 = numbers[i]
-        let num2 = target - num1
-        if (map.has(num2)) return [map.get(num2)
-            + 1, i + 1]
-
-        map.set(numbers[i], i)
-        i++
+    let left = 0
+    let right = numbers.length -1
+    while(left < right){
+        let sum = numbers[left]+ numbers[right]
+        if(sum == target){
+            return [left+1,right+1]
+        }else if(sum > target){
+            right--
+        }else{
+            left++
+        }
     }
-    console.log(map)
-
 };
