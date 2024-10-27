ea2be8c24c8b1bf5b1f34167d0b5ff5fbc9a6691
Eslam Mahmoud <espython85@gmail.com>
2024-10-27 17:42:52 +0300

    Time: 6 ms (14.77%), Space: 59.7 MB (60.2%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,18 +4,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-    k %= nums.length // if k is greater than nums.length then one cycle is completed that means it will remain the same and we have to remainder shifts
-  
-    let reverse = function (start, end) {
+    k %= nums.length
+    function reverse(start, end) {
         while (start < end) {
-           [nums[start], nums[end]] = [nums[end],nums[start]]
+            [nums[start], nums[end]] = [nums[end], nums[start]]
             start++
             end--
         }
-    } // suppose  ----->---> 
-    let start =0 , end = nums.length -1 
-    reverse(start, end); // reverse   <--<------
-    reverse(start , k-1) // reverse first part ---><----
-    reverse(k, end)// reverse second part --->----->
+    }
+    let start = 0, end = nums.length - 1
+    reverse(start, end)
+    reverse(start, k - 1)
+    reverse(k, end)
+
+
 
 };
