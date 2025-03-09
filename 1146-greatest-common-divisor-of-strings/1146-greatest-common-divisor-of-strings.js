212762cf1665d222fe9c967a5c214a763dac0371
Eslam Mahmoud <espython85@gmail.com>
2025-03-09 02:51:26 +0200

    Time: 1 ms (35.57%), Space: 53.3 MB (33.44%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.js
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.js
@@ -4,24 +4,19 @@
  * @return {string}
  */
 var gcdOfStrings = function(str1, str2) {
-      if (str1 + str2 !== str2 + str1) {
-        return "";
-    }
-    
-    // Calculate the GCD of the lengths
-    const gcdLength = gcd(str1.length, str2.length);
-    
-    // Return the prefix of length gcdLength
-    return str1.substring(0, gcdLength);
+    if(str1+str2 !== str2+str1) return ""
+    let gcdNum = gcd(str1.length,str2.length)
+
+    return str1.substring(0,gcdNum)
 }
 
 /**
  * Helper function to find GCD of two numbers using Euclidean algorithm
  */
 function gcd(a, b) {
-    while (b) {
-        [a, b] = [b, a % b];
+    while(b){
+        [a,b] = [b,a%b]
     }
-    return a;
+    return a
     
 };
