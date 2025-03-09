d3f3df43e016e3f10a27701074b90f9410e21297
Eslam Mahmoud <espython85@gmail.com>
2025-03-09 23:45:38 +0200

    Time: 0 ms (100%), Space: 56 MB (5.52%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.js
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.js
@@ -4,20 +4,12 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    let max = 0
-    let exArray = []
-    for(let i =0 ; i < candies.length ; i++){
-        max = Math.max(candies[i],max)
-    }
+    let maxCandies = Math.max(...candies)
 
-    for(let i =0 ; i < candies.length ; i++){
-       if(candies[i] + extraCandies >= max){
-        exArray.push(true)
-       }else {
-        exArray.push(false)
-       }
-    }
-    
-return exArray
+    return candies.map(candy=>{
+        return candy + extraCandies >= maxCandies
+    }) 
+  
+
     
 };
