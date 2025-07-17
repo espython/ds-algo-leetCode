1d3916a0d4144cc50fbad25ce815e37b1a48dbaf
Eslam Mahmoud <espython85@gmail.com>
2025-07-17 23:55:44 +0300

    Time: 7 ms (58.68%), Space: 59.4 MB (36.38%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,25 +3,45 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let l = 0;
-    let length = s.length;
-    let set = new Set();
+    let l =0
+    let length = s.length
+    let set = new Set()
     let longest = 0;
-    
-    for(let r = 0; r < length; r++) {
-        // While the current character is already in our set
-        while(set.has(s[r])) {
-            // Remove the leftmost character and move the left pointer
-            set.delete(s[l]);
-            l++;
-        }
-        
-        // Add the current character to our set
-        set.add(s[r]);
-        
-        // Update the longest length if current window is longer
-        longest = Math.max(longest, r - l + 1);
+    for(let r =0 ; r < length ; r++){
+        while(set.has(s[r])){
+        set.delete(s[l])
+        l++
+    }
+        set.add(s[r])
+        longest = Math.max(longest,r-l +1)
     }
     
-    return longest;
+  return longest
+    
+    
+    
 };
+
+// var lengthOfLongestSubstring = function(s) {
+//     let l = 0;
+//     let length = s.length;
+//     let set = new Set();
+//     let longest = 0;
+    
+//     for(let r = 0; r < length; r++) {
+//         // While the current character is already in our set
+//         while(set.has(s[r])) {
+//             // Remove the leftmost character and move the left pointer
+//             set.delete(s[l]);
+//             l++;
+//         }
+        
+//         // Add the current character to our set
+//         set.add(s[r]);
+        
+//         // Update the longest length if current window is longer
+//         longest = Math.max(longest, r - l + 1);
+//     }
+    
+//     return longest;
+// };
