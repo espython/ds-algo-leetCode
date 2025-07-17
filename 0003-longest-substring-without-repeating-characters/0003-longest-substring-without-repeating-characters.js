9f3e3846dbd847627a7a871448094e919c98374a
Eslam Mahmoud <espython85@gmail.com>
2025-07-17 23:54:04 +0300

    Time: 8 ms (48.83%), Space: 58.4 MB (53.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,22 +3,25 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let l =0
-    let length = s.length
-    let set = new Set()
+    let l = 0;
+    let length = s.length;
+    let set = new Set();
     let longest = 0;
-    for(let r =0 ; r < length ; r++){
-        console.log({longest})
-        while(set.has(s[r])){
-        set.delete(s[l])
-        l++
+    
+    for(let r = 0; r < length; r++) {
+        // While the current character is already in our set
+        while(set.has(s[r])) {
+            // Remove the leftmost character and move the left pointer
+            set.delete(s[l]);
+            l++;
+        }
+        
+        // Add the current character to our set
+        set.add(s[r]);
+        
+        // Update the longest length if current window is longer
+        longest = Math.max(longest, r - l + 1);
     }
-        set.add(s[r])
-        longest = Math.max(longest,r-l +1)
-    }
     
-  return longest
-    
-    
-    
+    return longest;
 };
