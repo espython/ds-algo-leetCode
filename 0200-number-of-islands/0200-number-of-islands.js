ffcc60120e0b90f9b0bf235a4e6bbe7ffd8c0441
Eslam Mahmoud <espython85@gmail.com>
2025-08-08 01:38:07 +0300

    Time: 101 ms (14.44%), Space: 69.1 MB (12.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -2,40 +2,44 @@
  * @param {character[][]} grid
  * @return {number}
  */
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
 var numIslands = function(grid) {
+    if(!grid) return 0;
+
+    let rowsLength = grid.length; colsLength = grid[0].length
+    const visit = new Set();
     let islands = 0;
-    const visited = new Set();
-    const rows = grid.length;
-    const cols = grid[0].length;
 
-    const bfs = (r, c) => {
-        const q = [];
-        visited.add(`${r},${c}`);
-        q.push([r, c]);
-
-        while (q.length > 0) {
-            const [row, col] = q.shift();
-            const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
-
-            for (const [dr, dc] of directions) {
-                const nr = row + dr;
-                const nc = col + dc;
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] === "1" && !visited.has(`${nr},${nc}`)) {
-                    q.push([nr, nc]);
-                    visited.add(`${nr},${nc}`);
+    function bfs(r,c){
+        let q = []
+        visit.add(`${r},${c}`)
+        q.push([r,c])
+        while(q.length > 0){
+            let [row,col] = q.shift();
+            let directions = [[1,0],[0,1],[-1,0],[0,-1]]
+            for(let [dr,dc] of directions){
+                let r = row + dr, c = col + dc;
+            
+                if(r >= 0 && r < rowsLength && c >= 0 && c < colsLength && grid[r][c] == "1" && !visit.has(`${r},${c}`)){
+                    q.push([r,c])
+                    visit.add(`${r},${c}`)
                 }
-            }
-        }
-    };
-
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
-            if (grid[r][c] === "1" && !visited.has(`${r},${c}`)) {
-                islands += 1;
-                bfs(r, c);
             }
         }
     }
 
-    return islands;    
+    for(let r = 0; r<rowsLength ; r++){
+        for(let c = 0; c<colsLength; c++){
+            if(grid[r][c] == "1" && !visit.has(`${r},${c}`)){
+                bfs(r,c)
+                islands += 1
+            }
+        }
+    }
+
+return islands
+    
 };
