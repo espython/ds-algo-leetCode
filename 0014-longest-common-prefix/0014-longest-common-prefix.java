352ec2a389ea889f622fd39822e79dd925a7432f
Eslam Mahmoud <espython85@gmail.com>
2025-08-15 00:02:52 +0300

    Time: 1 ms (62.35%), Space: 41.5 MB (76.89%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+           if (strs == null || strs.length == 0) {
+            return "";
+        }
+        
+        int minLength = Integer.MAX_VALUE;
+        for (String s : strs) {
+           minLength = Integer.min(minLength, s.length());
+        }
+        
+        int i = 0;
+        while (i < minLength) {
+            for (String s : strs) {
+                if (s.charAt(i) != strs[0].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+            i++;
+        }
+        
+        return strs[0].substring(0, i);
+
+    }
+}
