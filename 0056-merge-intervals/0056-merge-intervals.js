7f32b5506b486840b8a30ec22b15ed4de64ea335
Eslam Mahmoud <espython85@gmail.com>
2025-09-01 22:30:08 +0300

    Time: 8 ms (71.45%), Space: 64.5 MB (68.77%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,14 +3,16 @@
  * @return {number[][]}
  */
 var merge = function (intervals) {
+    if (intervals.length === 0) return [];
+
     const merged = []
     intervals.sort((a, b) => a[0] - b[0])
-    const lastIndex = merged.length > 0 ? merged.length - 1 :0
+
     for (let interval of intervals) {
-        if (merged.length == 0 || merged[lastIndex][1] < interval[0]) {
+        if (merged.length == 0 || merged[merged.length - 1][1] < interval[0]) {
             merged.push(interval)
         } else {
-            merged[lastIndex][1] =  Math.max(merged[lastIndex][1], interval[1])
+            merged[merged.length - 1][1] = Math.max(merged[merged.length - 1][1], interval[1])
         }
     }
     return merged
