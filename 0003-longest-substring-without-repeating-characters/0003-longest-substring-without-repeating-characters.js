ea8784039ac2e43338eae980d1c33848c30f615b
Eslam Mahmoud <espython85@gmail.com>
2025-09-07 00:05:40 +0300

    Time: 4 ms (89.87%), Space: 56.1 MB (90.2%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -4,22 +4,28 @@
  */
 var lengthOfLongestSubstring = function(s) {
 
-    
     const set = new Set()
-    let l =0
-    let length = s.length
-    let longest = 0
-    for(let r = 0 ; r < length ; r++){
+    let l = 0;
+    let longest = 0;
+
+    for(let r =0 ; r < s.length ; r++){
         while(set.has(s[r])){
             set.delete(s[l])
             l++
         }
-
-        set.add(s[r])
-        longest = Math.max(longest,r-l+1)
+            
+       set.add(s[r])
+       longest = Math.max(longest , r-l+1)
+        
     }
-    
+
     return longest
+
+
+
+
+
+  
 };
 
 
